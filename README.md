0f8e7827918634dad0d9eed1890ab2c179d81501
eamoses <e.a.moses@gmail.com>
2019-06-27 16:17:35 -0500

    oauth login template repo

---
+++ README.md
@@ -0,0 +1,11 @@
+# Spotify Client Side OAuth API with ReactJS
+
+* This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+* Logo courtesy of: [By Fabián Alexis](https://github.com/fabianalexisinostroza/Antu)
+[CC BY-SA 3.0](https://commons.wikimedia.org/w/index.php?curid=49950811)
+
+## Spotify Web API
+
+https://developer.spotify.com/documentation/web-api/reference-beta
+
---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+import { authEndpoint, clientId, redirectUri, scopes } from "./config";
+import hash from "./hash";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: [],
+      token: null,
+      is_playing: "Paused",
+    };
+  }
+  componentDidMount() {
+    let _token = hash.access_token
+    if (_token) {
+      this.setState({
+        token: _token
+      });
+      this.getUserInfo(_token);
+    }
+  }
+  getUserInfo = (token) => {
+    fetch('https://api.spotify.com/v1/me/', {
+    headers: {
+      'Authorization': 'Bearer ' + token
+    },
+  })
+    .then(response => response.json())
+    .then(data =>
+    (
+      {
+        name: `${data.display_name}`,
+        username: `${data.id}`,
+        email: `${data.email}`,
+        location: `${data.country}`
+      }
+    )
+    )
+    .then(user => this.setState({
+      user,
+      is_playing: "Paused"
+    })
+    )
+    .catch(error=>console.log("There as an error", error))
+  }
+  render() {
+    const {user, is_playing} = this.state;
+    const {username, name, email, location} = user;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          { this.state.token ? (
+            <div className="user-details">
+              User's name:{name} <br></br>
+              Spotify username:{username}<br></br>
+              Email:{email}<br></br>
+              Location:{location}<br></br>
+              Playing or paused:{is_playing}
+            </div>
+          ) : (
+            <a
+              className="btn btn--loginApp-link"
+              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+            >
+              Log in
+            </a>
+            )
+          }
+        </header>
+      </div>
+    );
+  }
+}
+export default App;
